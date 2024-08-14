5b2e988e136d5c92f64af4c28b396bf3713f4dae
veleswaran <vels344@gmail.com>
2024-08-14 16:01:10 +0530

    attendance table

--- components/sections/user/ListUser.js
+++ components/sections/user/ListUser.js
@@ -18,7 +18,7 @@ export default function ListUser() {
       console.error('Error fetching data:', error);
       setError('Failed to fetch data.');
     }
-  }, []);
+  }, [dlt]);
 
   const deleteData = useCallback(async (id) => {
     try {
