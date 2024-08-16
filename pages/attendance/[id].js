ad76a0f82cf7d83e0f923dd64cf960354c43e552
veleswaran <vels344@gmail.com>
2024-08-16 17:30:06 +0530

    prisma integrated

--- pages/attendance/[id].js
+++ pages/attendance/[id].js
@@ -1,6 +1,30 @@
 
 import Layout from '../../components/layout/Layout';
 import EditUser from '../../components/sections/user/EditUser';
+import axios from 'axios';
+
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+
+  try {
+    const res = await axios.get(`http://localhost:3000/api/users/${id}`);
+    const data = res.data;
+
+    return {
+      props: {
+        user: data,
+        user_id: id,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    return {
+      props: {
+        error: 'Failed to fetch user data',
+      },
+    };
+  }
+}
 
 export default function UserPage({ user, user_id, error }) {
   return (
