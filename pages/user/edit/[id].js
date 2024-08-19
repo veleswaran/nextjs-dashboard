5a3b29d64d55169d8f7d4ff901e18a35c7c180b3
veleswaran <vels344@gmail.com>
2024-08-19 08:05:35 +0530

    edit

--- pages/user/edit/[id].js
+++ pages/user/edit/[id].js
@@ -16,7 +16,7 @@ export async function getServerSideProps(context) {
       },
     };
   } catch (error) {
-    console.error('Error fetching user data:', error);
+    console.log('Error fetching user data:', error);
     return {
       props: {
         error: 'Failed to fetch user data',
